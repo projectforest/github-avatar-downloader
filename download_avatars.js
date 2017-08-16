44d98c41fa699dfdc62a6130bc13e6a33ad90a62
hlinforest <hllin@sfu.ca>
2017-08-16 13:37:11 -0700

    request function

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,26 @@
 var request = require('request');
-var github_user = "projectforest";
-var github_token = "547921871e07d0dc15dcd89e4cc89c70066235c8";
+var github_user = 'projectforest';
+var github_token = '547921871e07d0dc15dcd89e4cc89c70066235c8';
+var fs = require('fs');
+var user_agent = github_user;
 
 
 console.log('Welcome to the github avatar downloader');
 
 
 function getRepoContributors(repoOwner, repoName, cb){
-  var request_url = "https://" + github_user + ':' + github_token + "@api.github.com/repos/" + repoOwner + '/' + repoName + '/contributors';
-  console.log(request_url); 
+  //let request_url = 'https://${github_user}:${github_token}@api.github.com/repos/${repoOwner}/${repoName}/contributors';
+  var request_url = 'https://'+ github_user + ':' + github_token+ '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  let options = {
+    url: request_url,
+    headers: { 'User-Agent': user_agent }
+  }
+
+  request.get(options, function(err, response, body){
+    if (err) throw err;
+    console.log(JSON.parse(body));
+  });
+
 }
 
 function cb(err, result){
@@ -16,3 +28,4 @@ function cb(err, result){
   console.log("Result: ", result);
 }
 getRepoContributors('nodejs', "node", cb);
+
